78d1e73279b36326190d87c7ce117154273feef9
Eduardo J Silva <silvaeduardojavier@hotmail.com>
2022-05-05 22:36:48 -0300

    solution14

--- src/main/java/com/portfolio/EduSilva/EduSilvaApplication.java
+++ src/main/java/com/portfolio/EduSilva/EduSilvaApplication.java
@@ -4,26 +4,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 import org.springframework.stereotype.Controller;
 
-//@Controller
 @SpringBootApplication
 @EntityScan(basePackageClasses = {
     EduSilvaApplication.class,
     Jsr310JpaConverters.class
 })
-public class EduSilvaApplication extends SpringBootServletInitializer {
+public class EduSilvaApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EduSilvaApplication.class, args);
     }
-    
-     @Override
-   protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-      return application.sources(EduSilvaApplication.class);
-   }
 
 }
